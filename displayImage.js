1cab4294304bcd45df8ee206cad8d0b9a508aa4b
Antonio Reda <70113532+AntoReda@users.noreply.github.com>
2023-12-08 01:33:16 -0500

    game is nearly finished. Missing bugfixes and testing

--- displayImage.js
+++ displayImage.js
@@ -3,11 +3,14 @@ var key2 = localStorage.getItem('key2');
 var key3 = localStorage.getItem('key3');
 var key4 = localStorage.getItem('key4');
 var key5 = localStorage.getItem('key5');
+var key6 = localStorage.getItem('key6');
 
 var pic1 = document.getElementById('Endings1');
 var pic2 = document.getElementById('Endings2');
 var pic3 = document.getElementById('Endings3');
 var pic4 = document.getElementById('Endings4');
+var pic5 = document.getElementById('Endings5');
+var pic6 = document.getElementById('Endings6');
 
 function updateDisplay(){
 
@@ -22,6 +25,18 @@ function updateDisplay(){
     if(localStorage.getItem("key3") === 'true')
     {
         pic3.style.display = 'inline';
+    }
+    if(localStorage.getItem("key4") === 'true')
+    {
+        pic4.style.display = 'inline';
+    }
+    if(localStorage.getItem("key5") === 'true')
+    {
+        pic5.style.display = 'inline';
+    }
+    if(localStorage.getItem("key6") === 'true')
+    {
+        pic6.style.display = 'inline';
     }
     
 }
--- engine.js
+++ engine.js
@@ -3,6 +3,7 @@ var key2 = localStorage.getItem('key2') === 'false';
 var key3 = localStorage.getItem('key3') === 'false';
 var key4 = localStorage.getItem('key4') === 'false';
 var key5 = localStorage.getItem('key5') === 'false';
+var key6 = localStorage.getItem('key6') === 'false';
 
 function appear1() {
     localStorage.setItem('key1', 'true'); // Update localStorage
@@ -24,12 +25,18 @@ function appear5() {
     localStorage.setItem('key5', 'true'); // Update localStorage
     updateDisplay();
 }
+function appear6() {
+    localStorage.setItem('key6', 'true'); // Update localStorage
+    updateDisplay();
+}
 
 function disappearAll() {
    localStorage.setItem('key1', 'false')
    localStorage.setItem('key2', 'false')
    localStorage.setItem('key3', 'false')
    localStorage.setItem('key4', 'false')
+   localStorage.setItem('key5', 'false')
+   localStorage.setItem('key6', 'false')
    updateDisplay();
 }
 function appearAll() {
@@ -37,5 +44,7 @@ function appearAll() {
     localStorage.setItem('key2', 'true')
     localStorage.setItem('key3', 'true')
     localStorage.setItem('key4', 'true')
+    localStorage.setItem('key5', 'true')
+    localStorage.setItem('key6', 'true')
     updateDisplay();
 }
